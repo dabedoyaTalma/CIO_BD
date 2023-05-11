85d3fda566424df128f86d75eda7867dcb3e81e4
dabedoyaLasa <51960004+dabedoyaLasa@users.noreply.github.com>
2023-05-11 12:22:33 -0500

    Update UFN_CIOWeb_BusinessRules_Additional_CertifiedOfficeService.sql

--- CIOReglaNegocio/Functions/UFN_CIOWeb_BusinessRules_Additional_CertifiedOfficeService.sql
+++ CIOReglaNegocio/Functions/UFN_CIOWeb_BusinessRules_Additional_CertifiedOfficeService.sql
@@ -10,6 +10,8 @@ GO
 -- =============================================================================================================================================================================	
 --	SELECT * FROM [CIOReglaNegocio].[UFN_CIOWeb_BusinessRules_Additional_CertifiedOfficeService](1333595, 1, 1, 1, 55, '2021-02-07', '2021-02-07 18:27', '2021-02-07 19:22')
 --
+--	2023-05-11	Diomer Bedoya	   : Se incluye RN TALMA-SAI SPIRIT  Compañía: SPIRIT - Facturar a: SAI
+
 ALTER FUNCTION [CIOReglaNegocio].[UFN_CIOWeb_BusinessRules_Additional_CertifiedOfficeService](--[CIOReglaNegocio].[UFN_CIOWeb_BusinessRules_CalculateSummedTime]
 	@ServiceHeaderId BIGINT,
 	@ServiceTypeId INT,
@@ -50,24 +52,68 @@ BEGIN
 		BEGIN
 			IF (@AirportId IN (3,4,17,9)) --MDE, CLO, CTG, AXM
 			BEGIN
-				INSERT @Result SELECT * FROM [CIOServicios].[UFN_CIOWeb_CalculateQuantity](@ServiceDetail, 0)
+				INSERT @Result 
+				SELECT 
+					CQ.StartTime
+				,	CQ.FinalTime
+				,	NULL
+				,	CQ.IsAdditionalService
+				,	NULL
+				,	NULL
+				,	NULL
+				,	0
+				,	CQ.AdditionalService
+				,	NULL
+				,	NULL
+				FROM [CIOServicios].[UFN_CIOWeb_CalculateQuantity](@ServiceDetail, 0) CQ
 				RETURN
 			END
 			
-			INSERT @Result SELECT * FROM [CIOReglaNegocio].[UFN_CIOWeb_BusinessRules_CalculateSummedTime](@ServiceDetail, 0, 30.0,NULL,NULL) 
-			RETURN
+			IF (@AirportId IN (30,32)) --BAQ, BGA
+			BEGIN
+				IF (@ServiceTime > 0)
+				BEGIN
+					INSERT	@Result
+					SELECT	R.StartTime,
+							R.EndTime,
+							CASE WHEN MAX(R.RowNumber) OVER(PARTITION BY Service) = R.RowNumber THEN SUM(R.TotalTime) OVER(PARTITION BY Service) END Time,
+							1 AdditionalService,
+							R.StartTime AdditionalStartTime,
+							R.EndTime AdditionalEndTime,
+							CASE WHEN MAX(R.RowNumber) OVER(PARTITION BY Service) = R.RowNumber THEN SUM(R.TotalTime) OVER(PARTITION BY Service) END AdditionalTime,
+							CASE WHEN MAX(R.RowNumber) OVER(PARTITION BY Service) = R.RowNumber THEN 1 END AdditionalQuanty,
+							R.Service + ' FIJO X 4 HORAS' AdditionalServiceName,
+							NULL FractionName,
+							NULL TimeLeftover
+					FROM	CIOReglaNegocio.ServiceDetailFilterByMinuteRanges(@ServiceDetail, 0, 240) R
+				END
+
+				IF (@ServiceTime > 240)
+				BEGIN
+					INSERT	@ServiceDetailFiltered
+					SELECT	RowNumber, Service, StartTime, EndTime, TotalTime, Quantity
+					FROM	CIOReglaNegocio.ServiceDetailFilterByMinuteRanges(@ServiceDetail, 120, NULL) --FRACCIONES DE 15 MIN ADICIONAL DESDE EL MINUTO 85 EN ADELANTE
+
+					INSERT  @Result
+					SELECT	StartDate
+						,	EndDate
+						,	Time
+						,	AdditionalService
+						,	AdditionalStartTime
+						,	AdditionalEndTime
+						,	AdditionalTime
+						,	AdditionalQuanty
+						,	AdditionalServiceName + ' ADICIONAL' AdditionalServiceName
+						,	FractionName
+						,	TimeLeftover
+					FROM    [CIOReglaNegocio].[UFN_CIOWeb_BusinessRules_CalculateSummedTime_V1](@ServiceDetailFiltered, 0, 60.0, '*', @ServiceHeaderId) --OJO NUEVA VERSION DE LA FUNCION
+					WHERE	AdditionalService = 1
+				END
+				RETURN
+			END
 		END
 	END
-	---- ==========================================
-	---- SARPA A SARPA
-	---- ==========================================
-	--IF (@CompanyId = 69 AND @BillingToCompany = 69)
-	--BEGIN
-	--	IF (@DateService >= '2021-10-01')
-	--	BEGIN
-	--		RETURN --SE INCLUYE EL DESPACHO CENTRALIZADO (NO SE COBRA ADICIONAL)
-	--	END
-	--END
 
+	INSERT @Result SELECT * FROM [CIOReglaNegocio].[UFN_CIOWeb_BusinessRules_CalculateSummedTime](@ServiceDetail, 0, 60.0, NULL,NULL)
 	RETURN
 END
